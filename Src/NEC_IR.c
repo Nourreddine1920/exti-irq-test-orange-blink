583667881355dcab634872a2c3b5a108f526621e
Nourreddine1920 <noureddine.awledbrahim@gmail.com>
2025-03-28 00:47:33 +0100

    feat: new updates in inc and src files

--- Src/NEC_IR.c
+++ Src/NEC_IR.c
@@ -18,7 +18,7 @@ void NEC_Decoder_Init(void)
   LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
   LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
   NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
-  
+
   SystemClock_Config();
 
   GPIO_Init();
@@ -26,30 +26,34 @@ void NEC_Decoder_Init(void)
   NEC_Timer_Init();
 }
 
-
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
 
-  if(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_2) {
-  Error_Handler();  
+  if (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_2)
+  {
+    Error_Handler();
   }
 
   LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
   LL_RCC_HSE_Enable();
 
   // Wait till HSE is ready
-  while(LL_RCC_HSE_IsReady() != 1) {};
+  while (LL_RCC_HSE_IsReady() != 1)
+  {
+  };
 
   LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_4, 64, LL_RCC_PLLP_DIV_2);
   LL_RCC_PLL_Enable();
 
   // Wait till PLL is ready
-  while(LL_RCC_PLL_IsReady() != 1) {};
+  while (LL_RCC_PLL_IsReady() != 1)
+  {
+  };
 
   LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
   LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
@@ -57,16 +61,17 @@ void SystemClock_Config(void)
   LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
 
   // Wait till System clock is ready
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL) {};
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  {
+  };
 
   LL_Init1msTick(64000000);
   LL_SetSystemCoreClock(64000000);
 }
 
-
 /**
-  * GPIO Initialization Function  
-  */
+ * GPIO Initialization Function
+ */
 static void GPIO_Init(void)
 {
   LL_EXTI_InitTypeDef EXTI_InitStruct = {0};
@@ -77,13 +82,9 @@ static void GPIO_Init(void)
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOD);
 
-  LL_GPIO_ResetOutputPin(GPIOD, LL_GPIO_PIN_12|LL_GPIO_PIN_13|LL_GPIO_PIN_14|LL_GPIO_PIN_15 
-                          |LL_GPIO_PIN_0|LL_GPIO_PIN_1|LL_GPIO_PIN_2|LL_GPIO_PIN_3 
-                          |LL_GPIO_PIN_4|LL_GPIO_PIN_5|LL_GPIO_PIN_6|LL_GPIO_PIN_7);
+  LL_GPIO_ResetOutputPin(GPIOD, LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15 | LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 | LL_GPIO_PIN_4 | LL_GPIO_PIN_5 | LL_GPIO_PIN_6 | LL_GPIO_PIN_7);
 
-  GPIO_InitStruct.Pin = LL_GPIO_PIN_12|LL_GPIO_PIN_13|LL_GPIO_PIN_14|LL_GPIO_PIN_15 
-                          |LL_GPIO_PIN_0|LL_GPIO_PIN_1|LL_GPIO_PIN_2|LL_GPIO_PIN_3 
-                          |LL_GPIO_PIN_4|LL_GPIO_PIN_6|LL_GPIO_PIN_7;
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15 | LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 | LL_GPIO_PIN_4 | LL_GPIO_PIN_6 | LL_GPIO_PIN_7;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
@@ -108,31 +109,28 @@ static void GPIO_Init(void)
   LL_GPIO_SetPinMode(IR_RECEIVER_PORT, IR_RECEIVER_PIN, LL_GPIO_MODE_INPUT);
 
   // EXTI1_IRQn interrupt configuration
-  NVIC_SetPriority(EXTI1_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),7, 0));
+  NVIC_SetPriority(EXTI1_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 7, 0));
   NVIC_EnableIRQ(EXTI1_IRQn);
 }
 
-
 /**
-  * Timer 3 Initialization Function
-  */
+ * Timer 3 Initialization Function
+ */
 static void NEC_Timer_Init(void)
 {
 }
 
-
-/* This function returns the NEC 
+/* This function returns the NEC
  * decoder to its initial state
  */
-void NEC_Timer_IRQ_Handler (void)
+void NEC_Timer_IRQ_Handler(void)
 {
 }
-
 
 /* This function handles
  * the IR receiver's output
  */
-void NEC_External_IRQ_Handler (void)
+void NEC_External_IRQ_Handler(void)
 {
   NVIC_ClearPendingIRQ(NEC_EXTI_IRQn);
   LL_EXTI_ClearFlag_0_31(IR_RECEIVER_LINE);
--- Src/main.c
+++ Src/main.c
@@ -1,29 +1,29 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
 /**
-  * The application entry point.
-  */
+ * The application entry point.
+ */
 int main(void)
 {
   NEC_Decoder_Init();
@@ -33,11 +33,10 @@ int main(void)
   }
 }
 
-
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
